ad972cc22fa24af14e0a8380acbe4c00402f5ded
gemmabecala@gmail.com <gemmabecala@gmail.com>
2018-04-20 13:51:21 +0200

    checking password

--- spec/mainspec.js
+++ spec/mainspec.js
@@ -1,11 +1,30 @@
 
 
-describe("the application", function(){
-    it("testing matches to understand values and expressions", function(){
+describe("testing matches to understand values and expressions", function(){
+    it("testing toBe", function(){
+                 
+                expect([1, 2, 3] === [1, 2, 3]).toBe(false);
+            
+                expect({ x: 1, y: 2, z: 3 } === { x: 1, y: 2, z: 3 }).toBe(false);
+            });
+            it("testing toEqual", function(){
                  
                 expect([1, 2, 3] === [1, 2, 3]).toEqual(false);
             
                 expect({ x: 1, y: 2, z: 3 } === { x: 1, y: 2, z: 3 }).toEqual(false);
             });
-         
-        })
+            
+            it("tests another arrays comparison", function () {
+                var array1 = [1, 2];
+                var array2 = [1, 2];
+                expect(array1 === array2).toEqual(false);
+                expect(array1 === array2).toBe(false);
+                expect(array1).toEqual(array2);
+                // expect(array1).toBe(array2); <- ojo, esto no se cumple
+            });
+        
+        
+
+
+
+        });
